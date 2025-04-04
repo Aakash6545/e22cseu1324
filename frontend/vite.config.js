4f70009d7f85f7295efc70f5adad2cadd88e349a
Aakash <aakashsheoran592@gmail.com>
2025-04-04 13:27:42 +0530

    configure Vite server

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(),tailwindcss()],
   server: {
+    
+    port: 3000,
+  
     proxy: {
       '/api': {
         target: 'http://localhost:3000',
